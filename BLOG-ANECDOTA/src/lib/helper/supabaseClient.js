0e570dd0b17b86e83be4a9c26d63bb828d55802b
Tomy Huisa <Huisatomy2007@gmail.com>
2024-09-16 16:41:57 -0300

    Arreglo de el supabase

--- BLOG-ANECDOTA/src/lib/helper/supabaseClient.js
+++ BLOG-ANECDOTA/src/lib/helper/supabaseClient.js
@@ -1,6 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
 
-const supabase = createClient('https://fivawgticowtvnhxifxu.supabase.co/', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpdmF3Z3RpY293dHZuaHhpZnh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5MDQ3MzIsImV4cCI6MjA0MTQ4MDczMn0.VtQQeKzOeaU5szv2HeMDBDkuHaSQl60vArbIhfZBw38')
+const supabase = createClient(
+  "https://hszemyfqyfdmxqwuhurs.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzemVteWZxeWZkbXhxd3VodXJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY0NTExOTksImV4cCI6MjA0MjAyNzE5OX0.bj8WwN7xqXQzME6vBIgB3-BDw2NmvpKUKbnM19Ie7KA"
+);
 export default supabase;
